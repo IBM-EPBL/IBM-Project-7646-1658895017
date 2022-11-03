2c0ab32d999aa906ff8c6a01ae67ba955e4a56bb
Devanand072001 <devanand.velu2001@gmail.com>
2022-11-03 22:09:58 +0530

    Added Assignment - 3

---
+++ Assignments/Team member - 1 Algore R/Assignment - 3/ObjectStorage/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect
+from object_config import object_files
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return render_template('index.html', files=object_files)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ Assignments/Team member - 1 Algore R/Assignment - 3/ObjectStorage/object_config.py
@@ -0,0 +1,54 @@
+import ibm_boto3
+from ibm_botocore.client import Config, ClientError
+
+# Constants for IBM COS values
+# Current list avaiable at https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints
+COS_ENDPOINT = "https://s3.jp-tok.cloud-object-storage.appdomain.cloud"
+# eg "W00YixxxxxxxxxxMB-odB-2ySfTrFBIQQWanc--P3byk"
+COS_API_KEY_ID = "Bc32PN-Ec2mpFRVtchtdfkBJBM-uDrjwYdY12L0knCH4"
+COS_INSTANCE_CRN = "crn:v1:bluemix:public:cloud-object-storage:global:a/fc99842817e447e4be38d2cca7268d9c:183cba6d-ad4f-4d17-b129-61f93943e367::"
+
+# Create resource
+cos = ibm_boto3.resource("s3",
+                         ibm_api_key_id=COS_API_KEY_ID,
+                         ibm_service_instance_id=COS_INSTANCE_CRN,
+                         config=Config(signature_version="oauth"),
+                         endpoint_url=COS_ENDPOINT
+                         )
+
+
+def get_buckets():
+    print("Retrieving list of buckets")
+    try:
+        buckets = cos.buckets.all()
+        for bucket in buckets:
+            # print("Bucket Name: {0}".format(bucket.name))
+            return bucket.name
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to retrieve list buckets: {0}".format(e))
+
+
+bucket_name = get_buckets()
+
+print(bucket_name)
+
+
+def get_bucket_contents(bucket_name):
+    print("Retrieving bucket contents from: {0}".format(bucket_name))
+    try:
+        file_names = []
+        files = cos.Bucket(bucket_name).objects.all()
+        for file in files:
+            # print("Item: {0} ({1} bytes).".format(file.key, file.size))
+            if 'img' in file.key:
+                file_names.append(file.key)
+        return file_names
+    except ClientError as be:
+        print("CLIENT ERROR: {0}\n".format(be))
+    except Exception as e:
+        print("Unable to retrieve bucket contents: {0}".format(e))
+
+
+object_files = get_bucket_contents(bucket_name)
